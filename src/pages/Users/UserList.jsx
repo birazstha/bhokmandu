ead88d3eba60be2c828536b7121ff71100c1fe64
biraj1234 <biraj.spell@gmail.com>
2024-08-10 20:49:08 +0545

    asdf

--- src/pages/Users/UserList.jsx
+++ src/pages/Users/UserList.jsx
@@ -13,8 +13,16 @@ export default function UserList(params) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-4 2xl:grid-cols-6 gap-4">
       {users.map((user) => (
-        <div className="border-2 flex flex-col gap-2 items-center justify-center p-4 rounded-lg">
-          <img src={user.profile_picture} alt="" className="rounded-full" />
+        <div className="flex flex-col gap-2 items-center justify-center p-4 border-2 shadow-lg rounded-lg">
+          <img
+            src={
+              user.profile_picture
+                ? user.profile_picture
+                : "https://st3.depositphotos.com/9998432/13335/v/450/depositphotos_133352010-stock-illustration-default-placeholder-man-and-woman.jpg"
+            }
+            alt=""
+            className="rounded-full h-20 border-2"
+          />
           <div className="text-center">
             <p>{user.name}</p>
             <p>{user.email}</p>
